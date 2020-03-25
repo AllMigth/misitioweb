25a4f8c3da4664cc44f9f75cb80e90a4a6dc1a3c
JuanJose <42131930+AllMigth@users.noreply.github.com>
2020-03-24 23:22:20 -0500

    Agregando la imagen y los colores

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,15 @@
 import React from "react"
+import ilustration from '../imgs/fondo.svg';
 
 export default() => (
-    <header className = "bg-purple-600">
+    <header className = "bg-gray-300">
         <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center"> 
             <div className="flex-1">
-            <h1>Hola! Soy Juan</h1>
-            <p> Creo aplicaciones moviles para android y diseno de ui</p>
+            <h1 className="font-bold text-purple-700 text-6xl" >¡Hola! Soy Juan</h1>
+            <p className="text-xl"> Creo aplicaciones moviles para android 
+            y diseño de interfaces de usuario, también hago copywritting.</p>
             </div>
-            <img></img>
+            <img src={ilustration} alt="Enviroment studie for one men" style={{height: "250px"}}></img>
         </div>
         
     </header>
