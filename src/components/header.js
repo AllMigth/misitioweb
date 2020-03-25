e8c2861ec74ecc10774a6f26d4137b980a9fb6e4
JuanJose <42131930+AllMigth@users.noreply.github.com>
2020-03-24 22:52:12 -0500

    Empezando la web, por Juan Garzon

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default() => (
+    <header className = "bg-purple-600">
+        <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center"> 
+            <div className="flex-1">
+            <h1>Hola! Soy Juan</h1>
+            <p> Creo aplicaciones moviles para android y diseno de ui</p>
+            </div>
+            <img></img>
+        </div>
+        
+    </header>
+)
+    
