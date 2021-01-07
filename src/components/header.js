47882feba45c27c41b4d849101a87587eae820a4
Juan Garzon <42131930+AllMigth@users.noreply.github.com>
2021-01-06 22:36:58 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -10,14 +10,11 @@ export default() => (
                     <h1 className="font-bold text-purple-700 text-6xl" >¡Hola! Soy Juan</h1>
                     <p className="text-xl"> Desarrollador de software para android y la web.</p>
                 </div>
-                <img src={ilustration} alt="Enviroment studie for one men" style={{height: "auto"}}></img>  
+                <img src={ilustration} alt="Enviroment studie for one men" style={{height: "150"}}></img>  
                 </div>
+     
     <div>
-    <h3 className="font-bold text-black text-xl" > Quiero construir mi portaofolio </h3>
-    </div> 
-    
-    <div>
-    <h3 className="font-bold text-black text-xl" > https://store-optafolio.netlify.app/ </h3>
+    <a href="https://store-optafolio.netlify.app/" className="text-xl"> Tienda </a>
     </div> 
     
         </div>
