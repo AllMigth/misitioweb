33a407d89eec811512b6852d2e6f3a3be376d700
Juan Garzon <42131930+AllMigth@users.noreply.github.com>
2021-02-26 10:24:41 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,8 @@ export default() => (
     <header className = " flex-1 bg-gray-300">
         <div className="container mx-auto p-12 max-w-4xl "> 
             <div className="flex justify-center items-center">
-                <div className="flex-1 h-full text-left">
-                    <h1 className="font-extrabold text-purple-700 text-3xl" >Soy Juan</h1>
+                <div className="flex-1">
+                    <h1 className="font-bold text-purple-700" >Soy Juan</h1>
                     <p className="text-xl font-light"> Desarrollador de software para android y la web.</p>
                 </div>
                 <img src={ilustration} alt="Enviroment studie for one men" style={{height: "180px"}}></img>  
@@ -17,7 +17,7 @@ export default() => (
     <a href="https://store-optafolio.netlify.app/" className="text-xl"> Tienda </a>
     </div> 
     
-        </div>
+    </div>
     </header>
 )
     
