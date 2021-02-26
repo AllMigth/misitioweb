12992cb3433d5fc383654d664be36427850154da
Juan Garzon <42131930+AllMigth@users.noreply.github.com>
2021-02-26 10:07:57 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,9 +6,9 @@ export default() => (
     <header className = " flex-1 bg-gray-300">
         <div className="container mx-auto p-12 max-w-4xl "> 
             <div className="flex justify-center items-center">
-                <div className="flex-1">
-                    <h1 className="font-bold text-purple-700 text-6xl" >¡Hola! Soy Juan</h1>
-                    <p className="text-xl"> Desarrollador de software para android y la web.</p>
+                <div className="flex-1 h-full text-left">
+                    <h1 className="font-extrabold text-purple-700 text-3xl" >¡Hola! Soy Juan</h1>
+                    <p className="text-xl font-light"> Desarrollador de software para android y la web.</p>
                 </div>
                 <img src={ilustration} alt="Enviroment studie for one men" style={{height: "180px"}}></img>  
                 </div>
